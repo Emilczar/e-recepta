7d81d1c1b69201017e122d67b9cecd9d3a12557d
Emilczar <emilczar@gmail.com>
2015-12-16 22:58:13 +0100

    dodanie dodatkowego widoku

--- webapp/src/main/java/com/packt/webapp/controller/SzukajControler.java
+++ webapp/src/main/java/com/packt/webapp/controller/SzukajControler.java
@@ -1,10 +1,13 @@
 package com.packt.webapp.controller;
 
+import com.packt.webapp.domain.Lek;
 import com.packt.webapp.service.BazaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SzukajControler
@@ -12,7 +15,7 @@ public class SzukajControler
   @Autowired
   private BazaService bazaService;
   
-  @RequestMapping({"/test"})
+  @RequestMapping({"/"})
   public String list(Model model)
   {
     String lek = "%Gabap%";
@@ -21,4 +24,23 @@ public class SzukajControler
     
     return "welcome";
   }
+  
+  
+  @RequestMapping(value ="/szukaj", method = RequestMethod.GET)
+  public String addLeki(Model model)
+  {
+   Lek newLek = new Lek();
+   model.addAttribute("newLek", newLek);
+   return "szukaj";
+  }
+  
+  @RequestMapping(value ="/szukaj", method = RequestMethod.POST)
+  public String getAddLek(@ModelAttribute("newLek") Lek newlek, Model model)
+  {
+	  String lek = newlek.getLek();
+	  model.addAttribute("lekLista", this.bazaService.findall(lek));
+   return "redirect:/szukaj";
+  
 }
+}
+
---
+++ webapp/src/main/webapp/WEB-INF/views/szukaj.jsp
@@ -0,0 +1,40 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+ 
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Contact Manager Home</title>
+    </head>
+    <body>
+        <div align="center">
+            <h1>Szukaj Leków</h1>
+			             
+        </div>
+       
+       
+       
+        <c:forEach var="emp" items="${leki}">
+        
+       <p> ${emp} </p>
+        </c:forEach>
+        <form:form method="post" modelAttribute=""> 
+		 <table>
+		<tr>
+		<td>lista</td>
+		
+		</tr>
+        
+        <tr>
+<td colspan="3"><input type="submit" value="Szukaj"/></td>
+	</tr>
+        
+        </table>
+    </form:form>
+        
+        
+    </body>
+</html>
